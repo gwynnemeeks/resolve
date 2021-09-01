d8cf11447a3ec811425e992756d5a75d499af651
Gwynne Meeks <gwynnemeeks@gmail.com>
2021-09-01 11:18:29 -0500

    wip intention component

---
+++ src/components/intention/Intention.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Intention.scss"
+
+export const Intention = () => (
+    <section className="intention">
+        <h3 className="intention__word">TRUE:</h3>
+        <div className="intention__date">Date: </div>
+    </section>
+)
