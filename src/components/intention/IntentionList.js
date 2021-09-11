a6703b24571f449f6bb2524573454a0b2036aa90
Gwynne Meeks <gwynnemeeks@gmail.com>
2021-09-11 12:58:44 -0500

    wip intention list

--- src/components/intention/IntentionList.js
+++ src/components/intention/IntentionList.js
@@ -1,9 +1,34 @@
-import React from "react"
+import React, { useContext, useEffect } from "react"
+import { IntentionContext } from "./IntentionProvider"
 import "./Intention.scss"
 
-export const IntentionList = () => (
-    <section className="intention">
-        <h3 className="intention__word">TRUE</h3>
-        <div className="intention__date">Date: </div>
-    </section>
-)
+export const IntentionList = () => {
+    // This state changes when `getIntentions()` is invoked below
+    const { intentions, getIntentions } = useContext(IntentionContext)
+
+    // useEffect - reach out to the world for something
+    // [] tells the useEffect when to run; here it is empty, so it runs on page load
+    useEffect(() => {
+        getIntentions()
+    }, [])
+
+    return (
+
+        <section className="intentions">
+            {
+                intentions.map(intention => {
+                    return (
+                        <div className="intention" id={`intention--${intention.id}`}>
+                            <div className="intention__word">
+                                <h3>Anchor: {intention.anchor} </h3>
+                            </div>
+                            <div className="intention__date">
+                                <h5>Date: {intention.date} </h5>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </section >
+    )
+}
